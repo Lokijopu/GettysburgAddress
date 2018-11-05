d08b3d4ab7ef3c16985f5871c0ea9952efb19b8e
Lokijopu <kguo2134@gmail.com>
2018-11-05 13:37:20 -0500

    Add files via upload

---
+++ src/ArrayList.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+/**
+ * 
+ * @author Kevin Guo Period 6 
+ *
+ */
+public class ArrayList {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		Scanner file = new Scanner(new File(""));
+        // assume filename stores the name of the file 
+	while (file.hasNext()) {
+		String word = file.next();
+	} 
+		
+
+	}
+
+}
